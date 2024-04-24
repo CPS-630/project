99ea55663763aa0f73f7bb07cdc094b2426dc4b5
Mathew-Estafanous <mathewestafanous13@gmail.com>
2024-04-24 18:31:48 -0400

    chore: use regular json logs when in prod environment

--- server/src/configs/logging.ts
+++ server/src/configs/logging.ts
@@ -1,18 +1,26 @@
-import pino from 'pino';
+import pino, { LoggerOptions } from 'pino';
+import { config } from 'dotenv';
 
-const LOGGER = pino({
+config();
+
+const logConfig: LoggerOptions = {
   name: 'tmu-connect-api',
   level: process.env.PINO_LOG_LEVEL || 'debug',
   timestamp: pino.stdTimeFunctions.isoTime,
   redact: {
     paths: ['email'],
   },
-  transport: {
+};
+
+if (process.env.ENVIRONMENT !== 'prod') {
+  logConfig.transport = {
     target: 'pino-pretty',
     options: {
       colorize: true,
     },
-  },
-});
+  };
+}
+
+const LOGGER = pino(logConfig);
 
 export default LOGGER;
