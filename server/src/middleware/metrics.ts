b03fb08132e82bc07263a80179b20c2aae6f3702
Mathew-Estafanous <mathewestafanous13@gmail.com>
2024-04-26 10:58:05 -0400

    chore: add url mask for auth0 ids

--- server/src/middleware/metrics.ts
+++ server/src/middleware/metrics.ts
@@ -4,6 +4,11 @@ const metricsMiddleware = express_prom_bundle({
   includeMethod: true,
   includePath: true,
   buckets: [0.001, 0.01, 0.1, 1, 2, 3, 5, 7, 10, 15, 20, 25, 30, 35, 40, 50, 70, 100, 200],
+  urlValueParser: {
+    extraMasks: [
+      /^.+(\||%7C).+/,
+    ],
+  },
 });
 
 export default metricsMiddleware;
